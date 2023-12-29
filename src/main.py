bdb4b6e4dea526f027d35071c6e0d152f2447d8d
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2023-12-29 11:15:49 -0500

    Add argparse and logging functionality in main.py
    The main.py script now includes argparse for command-line argument parsing and a logger setup. Additionally, the script now supports option tests and handles termination gracefully, with adequate notification of these events in the logger.

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import argparse
+import atexit
+from PcoLive import choose_live, exit_handler, get_index
+from utils import setup_logger
+
+
+def main():
+    logger = setup_logger(__name__)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--test", help="test option: return the string 'SUCCESS'", action='store_true')
+    parser.add_argument("-s", "--service_type_id", help="service type id")
+    parser.add_argument("-p", "--plan_id", help="plan id")
+
+    args = parser.parse_args()
+
+    if args.test:
+        print("SUCCESS")
+        return
+    if args.service_type_id and args.plan_id:
+        try:
+            config = choose_live(args.service_type_id, args.plan_id)
+            atexit.register(exit_handler, config['service_type_id'], config['plan_id'])
+            get_index(config['service_type_id'], config['plan_id'])
+        except KeyboardInterrupt:
+            logger.info("Thanks for using this recipe. Check out more recipes at https://pcochef.com")
+    else:
+        try:
+            config = choose_live()
+            atexit.register(exit_handler, config['service_type_id'], config['plan_id'])
+            get_index(config['service_type_id'], config['plan_id'])
+        except KeyboardInterrupt:
+            logger.info("Thanks for using this recipe. Check out more recipes at https://pcochef.com")
+
+
+if __name__ == "__main__":
+    main()
